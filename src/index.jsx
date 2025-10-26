3fe2281fe787783298b7324b15dfcf0b4ef7904d
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-10-25 20:33:34 -0600

    fix: elimina importacion de react por defecto

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+createRoot(document.getElementById('root')).render(<App />)
